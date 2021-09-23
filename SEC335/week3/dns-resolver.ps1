6e3f94ecb435657e7ab18e6feeb05d46df8f24d9
ecrawford-0 <61421800+ecrawford-0@users.noreply.github.com>
2021-09-22 22:47:02 -0400

    Delete dns-resolver.ps1

--- SEC335/week3/dns-resolver.ps1
+++
@@ -1,7 +0,0 @@
-param($network, $server)
-for ($i =0; $i -lt 255; $i++)
-{
-
-$ip = "$network" + "." +  "$i"
-Resolve-DnsName -DnsOnly $ip -Server $server -ErrorAction Ignore | Select-Object -Property Name, NameHost
-}  
