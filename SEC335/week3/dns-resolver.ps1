114a4e06700507461b3ca282036a82c47c3e28e8
ecrawford-0 <61421800+ecrawford-0@users.noreply.github.com>
2021-09-22 22:47:28 -0400

    Update and rename dns-resolver-improved.ps1 to dns-resolver.ps1

--- SEC335/week3/dns-resolver.ps1
+++ SEC335/week3/dns-resolver.ps1
@@ -1,16 +1,12 @@
+
 param($network, $server)
 for ($i = 0; $i -lt 255; $i++)
 {
 $ip = "$network" + "." +  "$i"
-
-
 $result = Resolve-DnsName -DnsOnly $ip -Server $server -ErrorAction Ignore | Select-Object -Property NameHost
-
 if ($result  -like "*")
-{
-    echo "ip:"$ip 
-    echo "server" $result
-    echo ""
+{ 
+    echo "IP: " $ip "namehost:" $result
+    
 }
- 
 }
